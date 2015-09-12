c856a161b4d194f28bd06dc6b9c45bd6903ee832
naotake.k <naotake.87@gmail.com>
2015-09-12 23:03:02 +0900

    サンプルアプリが動くまで何とかこぎつけた

--- app/src/main/java/com/example/ble/MainActivity.java
+++ app/src/main/java/com/example/ble/MainActivity.java
@@ -4,6 +4,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
@@ -18,6 +19,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -25,15 +27,28 @@ import android.widget.Toast;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.UUID;
+
 
 public class MainActivity extends ActionBarActivity implements CompoundButton.OnCheckedChangeListener {
 
+    public static final UUID ALERT_SERVICE_UUID = UUID.fromString("00001802-0000-1000-8000-00805f9b34fb");
+    public static final UUID BATTERY_SERVICE_UUID = UUID.fromString("0000180f-0000-1000-8000-00805f9b34fb");
+    public static final UUID BATTERY_UUID = UUID.fromString("00002a19-0000-1000-8000-00805f9b34fb");
+    public static final UUID BATTERY_POWER_STATE_UUID = UUID.fromString("00002a1b-0000-1000-8000-00805f9b34fb");
+    public static final UUID ALERT_LEVEL_UUID = UUID.fromString("00002a06-0000-1000-8000-00805f9b34fb");
+    public static final UUID LINK_LOSS_UUID = UUID.fromString("00002a06-0000-1000-8000-00805f9b34fb");
+
     private static final long SCAN_PERIOD = 10000;
-    private static final String TARGET_PERIPHERAL_NAME = "Team7PrototypeBLE";
+    private static final String TARGET_PERIPHERAL_NAME = "BSHSBTPT01BK";
+
+    private static final String TAG = "WakeUpTaroPrototype";
 
     private BluetoothAdapter bluetoothAdapter;
     private Handler handler;
     private BluetoothGatt bluetoothGatt;
+
+    private int pushCount;
 
     private BluetoothGattCallback gattCallback = new BluetoothGattCallback() {
 
@@ -45,19 +60,33 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
         @Override
         public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
             super.onConnectionStateChange(gatt, status, newState);
-            Log.d("onConnectionStateChange", status + "->" + newState);
+
+            Log.d(TAG, String.format("onConnectionStateChange Current status(%s) to New Status(%s).", status, newState));
 
             if (newState == BluetoothProfile.STATE_CONNECTED) {
-                Log.d("GATT", newState + ": " + BluetoothProfile.STATE_CONNECTED);
+                Log.d(TAG, "GATT New status is STATE_CONNECTED");
 
-//                gatt.discoverServices();
                 bluetoothGatt.discoverServices();
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
-                Log.d("GATT", newState + ": " + BluetoothProfile.STATE_DISCONNECTED);
+                Log.d(TAG, "GATT New status is STATE_DISCONNECTED");
 
+                //  ステータス変更
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changeScanStatus("Lost...");
+                        switchCharacteristicActionStatus(false);
+                        initializePushCount();
+                    }
+                });
             }
         }
 
+        /**
+         * {@link BluetoothGatt#discoverServices()}の呼び出し完了後に呼び出される。
+         *
+         * @see BluetoothGatt#discoverServices()
+         */
         @Override
         public void onServicesDiscovered(BluetoothGatt gatt, int status) {
             Log.d("onConnectionStateChange", "status -> " + status);
@@ -69,32 +98,89 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
                 }
                 Log.d("BluetoothGattService", "UUID is " + service.getUuid().toString());
             }
+
+            //  ステータス変更
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    changeScanStatus("Found!!");
+                    switchCharacteristicActionStatus(true);
+                    changePushCount("0");
+                }
+            });
+
+            // ボタン押下の通知受け取り設定
+            if (isConnected()) {
+                BluetoothGattCharacteristic c = characteristic(BATTERY_SERVICE_UUID, BATTERY_POWER_STATE_UUID);
+                bluetoothGatt.setCharacteristicNotification(c, true);
+            }
+        }
+
+        @Override
+        public void onCharacteristicRead(BluetoothGatt gatt, final BluetoothGattCharacteristic characteristic, int status) {
+            if (characteristic.getUuid().equals(BATTERY_UUID)) {
+                Log.d(TAG, "onCharacteristicRead battery: " + characteristic.getValue()[0]);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        changeBatteryStatus(String.format("%d%%", characteristic.getValue()[0]));
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            if (BATTERY_POWER_STATE_UUID.equals(characteristic.getUuid())) {
+                Log.d(TAG, "onCharacteristicChanged button: " + characteristic.getValue()[0]);
+
+                if (characteristic.getValue()[0] == 1) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            incrementPushCount();
+                        }
+                    });
+                }
+            }
         }
     };
 
     private BluetoothAdapter.LeScanCallback scanCallback = new BluetoothAdapter.LeScanCallback() {
         @Override
-        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
-            ParcelUuid[] uuids = device.getUuids();
-            String uuid = "";
-            if (uuids != null) {
-                Log.d("UUID", uuids.toString());
-                uuid += StringUtils.join(uuids, " ");
-            }
-            Log.d("ScanRecord", toStringOfUUID(scanRecord));
-            Log.d("BLEActivity", toStringOfDevice(device) + "[" + uuid + "]");
+        public void onLeScan(final BluetoothDevice device, int rssi, final byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    ParcelUuid[] uuids = device.getUuids();
+                    String uuid = "";
+                    if (uuids != null) {
+                        Log.d("UUID", uuids.toString());
+                        uuid += StringUtils.join(uuids, " ");
+                    }
+                    Log.d("BLEActivity", toStringOfDevice(device) + "[" + uuid + "]");
 
-            if (StringUtils.equals(device.getName(), TARGET_PERIPHERAL_NAME)) {
-                Log.d("DeviceName", TARGET_PERIPHERAL_NAME);
+                    if (StringUtils.equals(device.getName(), TARGET_PERIPHERAL_NAME)) {
+                        Log.d(TAG, "DeviceName: " + TARGET_PERIPHERAL_NAME);
 
-                // 意中のデバイスが見つかったらスキャンを停止
-                bluetoothAdapter.stopLeScan(scanCallback);
-                // changeScanStatus("Stop!");
+                        bluetoothGatt = device.connectGatt(getApplicationContext(), false, gattCallback);
 
-                bluetoothGatt = device.connectGatt(getApplicationContext(), false, gattCallback);
-            }
+                        if (bluetoothGatt == null) {
+                            Log.d(TAG, "BluetoothGATT is null");
+                        } else {
+                            Log.d(TAG, "BluetoothGATT is not null!!!!");
+                        }
+                    }
+                }
+            });
         }
 
+        /**
+         * {@link BluetoothDevice}の概要を表す文字列表現を返す。
+         *
+         * @param device スキャンして見つかった {@link BluetoothDevice}
+         * @return {@link BluetoothDevice}の概要
+         */
         private String toStringOfDevice(BluetoothDevice device) {
             StringBuilder sb = new StringBuilder();
             sb = sb.append("name=").append(device.getName());
@@ -102,58 +188,6 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
             sb = sb.append(", address=").append(device.getAddress());
             sb = sb.append(", type=").append(device.getType());
             return sb.toString();
-        }
-
-        private String toStringOfUUID(byte[] scanRecords) {
-            String uuid = "NONE";
-            if (scanRecords.length > 30) {
-                uuid = "Over 30 Length is " + scanRecords.length;
-
-                StringBuilder sb = new StringBuilder();
-                for (byte record : scanRecords) {
-                    sb.append(intToHex2(record & 0xff));
-                    sb.append(" ");
-                }
-                uuid += sb.toString();
-
-                /*
-                //iBeacon の場合 6 byte 目から、 9 byte 目はこの値に固定されている。
-                if ((scanRecords[5] == (byte) 0x4c) && (scanRecords[6] == (byte) 0x00) &&
-                        (scanRecords[7] == (byte) 0x02) && (scanRecords[8] == (byte) 0x15)) {
-                    uuid = intToHex2(scanRecords[9] & 0xff)
-                            + intToHex2(scanRecords[10] & 0xff)
-                            + intToHex2(scanRecords[11] & 0xff)
-                            + intToHex2(scanRecords[12] & 0xff)
-                            + "-"
-                            + intToHex2(scanRecords[13] & 0xff)
-                            + intToHex2(scanRecords[14] & 0xff)
-                            + "-"
-                            + intToHex2(scanRecords[15] & 0xff)
-                            + intToHex2(scanRecords[16] & 0xff)
-                            + "-"
-                            + intToHex2(scanRecords[17] & 0xff)
-                            + intToHex2(scanRecords[18] & 0xff)
-                            + "-"
-                            + intToHex2(scanRecords[19] & 0xff)
-                            + intToHex2(scanRecords[20] & 0xff)
-                            + intToHex2(scanRecords[21] & 0xff)
-                            + intToHex2(scanRecords[22] & 0xff)
-                            + intToHex2(scanRecords[23] & 0xff)
-                            + intToHex2(scanRecords[24] & 0xff);
-
-                    String major = intToHex2(scanRecords[25] & 0xff) + intToHex2(scanRecords[26] & 0xff);
-                    String minor = intToHex2(scanRecords[27] & 0xff) + intToHex2(scanRecords[28] & 0xff);
-                }
-                */
-            }
-
-            return uuid;
-        }
-
-        public String intToHex2(int i) {
-            char hex_2[] = {Character.forDigit((i >> 4) & 0x0f, 16), Character.forDigit(i & 0x0f, 16)};
-            String hex_2_str = new String(hex_2);
-            return hex_2_str.toUpperCase();
         }
     };
 
@@ -167,12 +201,20 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
             finish();
         }
 
-//        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
         handler = new Handler(getApplicationContext().getMainLooper());
 
         initializeBluetoothStatus();
+
+        // ラベル初期化
+        if (bluetoothAdapter.isEnabled()) {
+            changeScanStatus("Ready?");
+        } else {
+            changeScanStatus("---");
+        }
+        changeBatteryStatus("---");
+        initializePushCount();
     }
 
     /**
@@ -190,6 +232,9 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
             bluetoothAdapter.enable();
         } else {
             bluetoothAdapter.disable();
+            if (isConnected()) {
+                bluetoothGatt.close();
+            }
         }
     }
 
@@ -223,11 +268,13 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                bluetoothAdapter.startLeScan(scanCallback);
-//                UUID[] targets = {UUID.fromString("280611F4-7398-41F0-A209-091B226F51DD"), UUID.fromString("85F203DC-A35F-471A-B57A-2B2A772A4295")};
-//                bluetoothAdapter.startLeScan(targets, scanCallback);
+                if (isConnected()) {
+                    bluetoothAdapter.stopLeScan(scanCallback);
+                }
             }
         }, SCAN_PERIOD);
+
+        bluetoothAdapter.startLeScan(scanCallback);
     }
 
     public void stopScan(View view) {
@@ -238,13 +285,108 @@ public class MainActivity extends ActionBarActivity implements CompoundButton.On
         bluetoothAdapter.stopLeScan(scanCallback);
     }
 
-    public void sendSignal(View view) {
-        TextView text = (TextView) findViewById(R.id.labelReceiveData);
-        text.setText("Send!!");
+    private void switchCharacteristicActionStatus(boolean enabled) {
+        Log.d(TAG, "Switch Buttons for Characteristic action to " + enabled);
+
+        // 各種ボタンの状態変更
+        switchButtonStatus(R.id.writeAlarm, enabled);
+        switchButtonStatus(R.id.writeVibration, enabled);
+        switchButtonStatus(R.id.writeStop, enabled);
+        switchButtonStatus(R.id.readBattery, enabled);
+
+        changeBatteryStatus((enabled ? "Ready?" : "---"));
+    }
+
+    private void switchButtonStatus(int buttonId, boolean enabled) {
+        Button button = (Button) findViewById(buttonId);
+        button.setEnabled(enabled);
+    }
+
+    public void doWriteAlarm(View view) {
+        Log.d(TAG, "Do characteristic write [Alarm].");
+
+        boolean result = doWriteCharacteristic(view, 2);
+        Log.d(TAG, "Write [Alarm] characteristic result " + result);
+    }
+
+    public void doWriteVibration(View view) {
+        Log.d(TAG, "Do characteristic write [Vibration].");
+
+        boolean result = doWriteCharacteristic(view, 1);
+        Log.d(TAG, "Write [Vibration] characteristic result " + result);
+    }
+
+    public void doWriteStop(View view) {
+        Log.d(TAG, "Do characteristic write [Stop].");
+
+        boolean result = doWriteCharacteristic(view, 0);
+        Log.d(TAG, "Write [Stop] characteristic result " + result);
+    }
+
+    private boolean doWriteCharacteristic(View view, int level) {
+        if (!isConnected()) {
+            Log.d(TAG, "Lost connection...");
+            return false;
+        }
+
+        BluetoothGattCharacteristic c = characteristic(ALERT_SERVICE_UUID, ALERT_LEVEL_UUID);
+        c.setValue(new byte[]{(byte) level});
+
+        return bluetoothGatt.writeCharacteristic(c);
+    }
+
+    public void doReadBattery(View view) {
+        Log.d(TAG, "Do characteristic read [Battery].");
+
+        BluetoothGattCharacteristic c = characteristic(BATTERY_SERVICE_UUID, BATTERY_UUID);
+
+        boolean result = bluetoothGatt.readCharacteristic(c);
+        Log.d(TAG, "Read [Battery] characteristic result " + result);
     }
 
     private void changeScanStatus(String changedStatus) {
         TextView text = (TextView) findViewById(R.id.labelConnectStatus);
         text.setText(changedStatus);
+    }
+
+    private void changeBatteryStatus(String changedStatus) {
+        TextView text = (TextView) findViewById(R.id.labelBattery);
+        text.setText(changedStatus);
+    }
+
+    private void initializePushCount() {
+        pushCount = 0;
+        changePushCount("---");
+    }
+
+    private void incrementPushCount() {
+        pushCount++;
+        changePushCount(String.valueOf(pushCount));
+    }
+
+    private void changePushCount(String count) {
+        TextView text = (TextView) findViewById(R.id.labelPushCount);
+        text.setText(count);
+    }
+
+    private boolean isConnected() {
+        return (bluetoothGatt != null);
+    }
+
+    private BluetoothGattCharacteristic characteristic(UUID sid, UUID cid) {
+        if (!isConnected()) {
+            return null;
+        }
+        BluetoothGattService s = bluetoothGatt.getService(sid);
+        if (s == null) {
+            Log.w(TAG, "Service NOT found :" + sid.toString());
+            return null;
+        }
+        BluetoothGattCharacteristic c = s.getCharacteristic(cid);
+        if (c == null) {
+            Log.w(TAG, "Characteristic NOT found :" + cid.toString());
+            return null;
+        }
+        return c;
     }
 }
